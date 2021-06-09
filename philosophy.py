bf8cb309f63e1e50fa6edef9fdd3032d12527470
L3G4CY <70349799+L3G4Cyy@users.noreply.github.com>
2021-06-09 18:42:43 +0200

    Update philosophy.py

--- philosophy.py
+++ philosophy.py
@@ -1,29 +1,48 @@
+import urllib2
+import sys
 from bs4 import BeautifulSoup
-import requests
 
-page = input('Enter the name of the term you want to start from (instead of spaces put "_" (test_test) >> ')
+key = raw_input("Enter the starting article : ")
+link = "http://en.wikipedia.org/wiki/" + key
 
-html = requests.get('https://en.wikipedia.org/wiki/'+page)
-soup = BeautifulSoup(html)
-links = []
-n = 0
+url = urllib2.urlopen(link)
 
-while True:
-    for i in sites: 
-        if n == 0:
-            n += 1
-            continue
-        else:
-            n += 1
 
-            soup = requests.get
-            
-        for a in soup.find_all('a', href=True):
-            if a == 'Philosophy':
-                links.append(a)
-                break
-            else:
-                links.append(a)
+x = ""
+i = 1
+while (x!="Philosophy"):
+	soup  = BeautifulSoup(url)
+
+	var = soup.find("div", {"id":"mw-content-text"})
+	crawl_to = var.find("p").find_all("a")
+	
+	for links in crawl_to:
+		if (str(links.get("title"))=="None" or str(links.get("title"))=="Latin language" or str(links.get("title"))=="Greek language"):
+			pass
+		else:
+			y=str(links.get("title"))
+			#print y
+			if (len(y.split())>=2):
+				word =''
+				for x in range(len(y.split())):
+					word = word + y.split()[x]
+					if (x!=(len(y.split())-1)):
+						word=word+"_"
+				y=word
+
+
+			next_link = "http://en.wikipedia.org/wiki/" + y
+			#print next_link
+			x=str(links.get("title"))	
+			print x , ("(%i)" %i)
+			i+=1
+			if (x!="Philosophy"):
+				print ("   |")
+			if(i==100):
+				print "Quitting"
+				sys.exit(1)
+			url = urllib2.urlopen(next_link)
+			break
                     
 
         
